7f8701888231bffbe585fd7032cf55e647fbd01f
Youssef <youssefyaser2020@gmail.com>
2025-04-24 15:35:43 +0200

    Handled FormComponent HTML with bootstrap and ListComponent too

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interface/product';
 
 @Component({
   selector: 'app-product-list',
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-
+  productList: Product[] = [
+    {
+      id: 1,
+      name: 'Mouse',
+      description: 'This mouse is good',
+      rate: 7.5,
+      imageURL: 'https://fakeimg.pl/300/',
+    },
+    {
+      id: 2,
+      name: 'Keyboard',
+      description: 'This keyboard is good',
+      rate: 5.5,
+      imageURL: 'https://fakeimg.pl/300/',
+    },
+  ];
 }
