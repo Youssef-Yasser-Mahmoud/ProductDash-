341ab42da7921fe2ae84a14022d4dacd74583e58
Youssef <youssefyaser2020@gmail.com>
2025-04-24 15:37:21 +0200

    Handled FormComponent HTML with bootstrap and ListComponent too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, ProductFormComponent],
+  imports: [ProductListComponent, ProductFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
